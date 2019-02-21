e857fd54b443c44abfd6d89b3ea8987db36efd0f
Amr-Fekry-Bosila <amrfekryy@gmail.com>
2019-02-21 18:12:03 +0200

    First commit: distribution code

---
+++ application.py
@@ -0,0 +1,14 @@
+import os
+
+from flask import Flask
+from flask_socketio import SocketIO, emit
+
+# configure file to use flask and flask_socketio
+app = Flask(__name__)
+app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
+socketio = SocketIO(app)
+
+
+@app.route("/")
+def index():
+    return "Project 2: TODO"
