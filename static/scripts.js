9e554002dc3ee01226054ac20122a62415c70273
Amr-Fekry-Bosila <amrfekryy@gmail.com>
2019-02-26 23:23:46 +0200

    Validate displayname onkeyup, form onsubmit

--- static/scripts.js
+++ static/scripts.js
@@ -1,18 +1,81 @@
        
 document.addEventListener('DOMContentLoaded', () => {
 
-  // disable exit on Esc key or screen click
-  $('#exampleModal').modal({
-    keyboard: false,
-    backdrop: 'static'
-  })
-  
-  // show modal
+  // disable esc/click exit and show modal
+  $('#exampleModal').modal({keyboard: false, backdrop: 'static'})
   $('#exampleModal').modal('show')
 
   // keep messages scroll at bottom
   var messageBody = document.querySelector('#msg-body');
   messageBody.scrollTop = messageBody.scrollHeight - messageBody.clientHeight;
+
+  // validate if display name available onkeyup (mark is-valid/is-invalid)
+  var display_name = document.querySelector('#display_name');
+  var display_name_feedback = document.querySelector('#display_name_feedback');
+  display_name.onkeyup = () => {
+    // Get display_name value
+    const name = display_name.value;
+    // Initialize new request
+    const request = new XMLHttpRequest();
+    request.open('POST', '/validate_name'); 
+    // Callback function for when request completes
+    request.onload = () => {
+        // Extract JSON data from request
+        const data1 = JSON.parse(request.responseText);
+        // Update the result div
+        if (data1.name_available) {
+            // mark as valid
+            display_name.classList.remove('is-invalid');
+            display_name.classList.add('is-valid');
+            display_name_feedback.classList.remove('invalid-feedback');
+            display_name_feedback.classList.add('valid-feedback');
+            display_name_feedback.innerHTML = "Looks good!"
+        }
+        else {
+            // mark as invalid
+            display_name.classList.remove('is-valid');
+            display_name.classList.add('is-invalid');
+            display_name_feedback.classList.remove('valid-feedback');
+            display_name_feedback.classList.add('invalid-feedback');
+            display_name_feedback.innerHTML = "This name is taken!"
+        }
+        // check for empty field
+        if (display_name.value === "") {
+            // mark as invalid
+            display_name.classList.remove('is-valid');
+            display_name.classList.add('is-invalid');
+            display_name_feedback.classList.remove('valid-feedback');
+            display_name_feedback.classList.add('invalid-feedback');
+            display_name_feedback.innerHTML = "Please enter a display name"
+        }
+    }
+    // Add data to send with request
+    const data = new FormData();
+    data.append('display_name', name);
+    // Send request
+    request.send(data);
+    return false; // stop page reload
+  };
+  // verify start-form onsubmit
+  document.querySelector('#start_form').onsubmit = () => {
+      // temp variable
+      var invalid_name = false;        
+      // don't submit and mark invalid if:
+      // no display name present
+      if (display_name.value === "") {
+          display_name.classList.add('is-invalid');
+          display_name_feedback.classList.remove('valid-feedback');
+          display_name_feedback.classList.add('invalid-feedback');
+          display_name_feedback.innerHTML = "Please enter a display name"                           
+          invalid_name = true;
+      };
+      // invalid display name present
+      if (display_name.classList.contains('is-invalid')) {
+          invalid_name = true;
+      }
+      if (invalid_name) {return false;} else {return true;};
+  };
+
 
   // toggle sidebar on click
   document.querySelector('#sidebarCollapse').onclick = () => {
